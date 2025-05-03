f35ad71c10f946c8569302a621faf71db6ae8f98
abdelrahman-a99 <a.ahmed2299>
2025-05-03 04:27:22 +0300

    fixing the eslint error in the middleware.ts file

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export default function middleware(request: NextRequest) {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default function middleware(_request: NextRequest) {
     // your middleware logic
     return NextResponse.next();
 }
